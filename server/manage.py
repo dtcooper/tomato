ccd1d63eba1608622c0cc10a3c030dda95717307
David Cooper <david@dtcooper.com>
2022-09-08 18:40:04 -0700

    project structure change

--- server/manage.py
+++ server/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tomato.settings')
     try:
         from django.core.management import execute_from_command_line
         from django.core.management.commands.runserver import Command as RunserverCommand
--- server/tomato/settings.py
+++ server/tomato/settings.py
@@ -39,7 +39,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'tomato.urls'
 
 TEMPLATES = [
     {
