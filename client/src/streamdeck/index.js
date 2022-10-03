8207a23806b2bf7cf72b1d54ff71d838323b6ac5
David Cooper <david@dtcooper.com>
2022-10-02 23:47:28 -0400

    streamdeck updates

--- client/src/streamdeck/index.js
+++ client/src/streamdeck/index.js
@@ -1,29 +1,55 @@
 import { openDevice } from '@elgato-stream-deck/webhid'
+import { VENDOR_ID, DEVICE_MODELS } from '@elgato-stream-deck/core'
 
 const randInt = (max) => Math.floor(Math.random() * max)
 
-// Fill the first button form the left in the first row with a solid red color. This is asynchronous.
-document.addEventListener('alpine:init', async () => {
-  const hidDevices = await navigator.hid.getDevices()
+const validProductIds = new Set(DEVICE_MODELS.map((spec) => spec.productId))
+const isStreamDeck = (device) => {
+  return device.vendorId === VENDOR_ID && validProductIds.has(device.productId)
+}
 
-  if (hidDevices.length > 0) {
-    const streamDeck = await openDevice(hidDevices[0])
-    for (let i = 0; i < streamDeck.NUM_KEYS; i++) {
-      await streamDeck.fillKeyColor(i, 0, 0, 0)
-    }
-
-    streamDeck.on('down', (keyIndex) => {
-      streamDeck.fillKeyColor(keyIndex, randInt(0xFF), randInt(0xFF), randInt(0xFF))
-      console.log('key %d down', keyIndex)
-    })
-
-    streamDeck.on('up', (keyIndex) => {
-      streamDeck.fillKeyColor(keyIndex, 0, 0, 0)
-      console.log('key %d up', keyIndex)
-    })
-
-    streamDeck.on('error', (error) => {
-      console.error(error)
-    })
+const setupStreamDeck = async () => {
+  const streamDecks = (await navigator.hid.getDevices()).filter(device => isStreamDeck(device))
+  if (streamDecks.length === 0) {
+    return null
   }
+
+  const streamDeck = await openDevice(streamDecks[0])
+  for (let i = 0; i < streamDeck.NUM_KEYS; i++) {
+    await streamDeck.fillKeyColor(i, 0, 0, 0xFF)
+  }
+  await streamDeck.setBrightness(100)
+
+  streamDeck.on('down', async (keyIndex) => {
+    await streamDeck.fillKeyColor(keyIndex, 0xFF, 0, 0)
+    console.log('key %d down', keyIndex)
+  })
+
+  streamDeck.on('up', async (keyIndex) => {
+    await streamDeck.fillKeyColor(keyIndex, 0, 0, 0xFF)
+    console.log('key %d up', keyIndex)
+  })
+
+  streamDeck.on('error', (error) => {
+    console.error(error)
+  })
+
+  return streamDeck
+}
+
+document.addEventListener('alpine:init', async () => {
+  let streamDeck = await setupStreamDeck()
+
+  navigator.hid.addEventListener('connect', async (event) => {
+    if (!streamDeck) {
+      streamDeck = await setupStreamDeck()
+    }
+  })
+
+  navigator.hid.addEventListener('disconnect', async (event) => {
+    if (streamDeck?.device?.device?.device === event.device) {
+      await streamDeck.close()
+      streamDeck = null
+    }
+  })
 })
