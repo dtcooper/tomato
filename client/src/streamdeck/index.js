78be93837d4d389790fab1a7741d8f634358deea
David Cooper <david@dtcooper.com>
2022-10-04 20:18:06 -0400

    streamdeck demo

--- client/src/streamdeck/index.js
+++ client/src/streamdeck/index.js
@@ -1,3 +1,4 @@
+import Alpine from 'alpinejs'
 import { openDevice } from '@elgato-stream-deck/webhid'
 import { VENDOR_ID, DEVICE_MODELS } from '@elgato-stream-deck/core'
 
@@ -7,10 +8,28 @@ const isStreamDeck = (device) => {
 }
 
 const [font, fontPath] = ['Undefined Medium Local', 'undefined-medium']
+const icons = {}
+
+const initialize = async () => {
+  const undefinedMedium = new window.FontFace(font, `url(../assets/fonts/${fontPath}.woff2)`)
+  await undefinedMedium.load()
+  document.fonts.add(undefinedMedium)
+
+  const iconLoadPromises = []
+
+  for (const iconName of ['tomato', 'mdi-play', 'mdi-pause']) {
+    const icon = new window.Image()
+    iconLoadPromises.push(new Promise((resolve) => {
+      icon.onload = resolve
+      icon.src = `../assets/icons/${iconName}.svg`
+    }))
+    icons[iconName] = icon
+  }
+  await Promise.all(iconLoadPromises)
+}
 
 const renderText = (text, iconSize, verticalPadding = 15, horizontalPadding = 5) => {
-  const canvas = document.createElement('canvas')
-  canvas.width = canvas.height = iconSize
+  const canvas = new window.OffscreenCanvas(iconSize, iconSize)
   const ctx = canvas.getContext('2d', { willReadFrequently: true })
 
   ctx.fillStyle = 'black'
@@ -39,8 +58,7 @@ const renderText = (text, iconSize, verticalPadding = 15, horizontalPadding = 5)
   ctx.fillText(text, horizontalPadding, fontHeight + verticalPadding / stretchFactor)
   ctx.restore()
 
-  const invertedCanvas = document.createElement('canvas')
-  invertedCanvas.width = invertedCanvas.height = iconSize
+  const invertedCanvas = new window.OffscreenCanvas(iconSize, iconSize)
   const invertedCtx = invertedCanvas.getContext('2d', { willReadFrequently: true })
 
   invertedCtx.drawImage(canvas, 0, 0)
@@ -53,7 +71,7 @@ const renderText = (text, iconSize, verticalPadding = 15, horizontalPadding = 5)
   return { canvas, invertedCanvas }
 }
 
-const setupStreamDeck = async () => {
+const setupStreamDeck = async (conn) => {
   const streamDecks = (await navigator.hid.getDevices()).filter(device => isStreamDeck(device))
   if (streamDecks.length === 0) {
     return null
@@ -63,27 +81,37 @@ const setupStreamDeck = async () => {
   const { canvas: playCanvas, invertedCanvas: playInvertedCanvas } = renderText('PLAY', streamDeck.ICON_SIZE)
   const { canvas: pauseCanvas, invertedCanvas: pauseInvertedCanvas } = renderText('PAUSE', streamDeck.ICON_SIZE)
   const { canvas: nextCanvas, invertedCanvas: nextInvertedCanvas } = renderText('NEXT', streamDeck.ICON_SIZE)
-  const tomatoSvg = new window.Image()
-  await new Promise((resolve) => {
-    tomatoSvg.onload = resolve
-    tomatoSvg.src = '../assets/icons/tomato.svg'
-  })
+  const playIconCanvas = new window.OffscreenCanvas(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
+  let ctx = playIconCanvas.getContext('2d', { willReadFrequently: true })
+  ctx.fillStyle = '#36D399'
+  ctx.fillRect(0, 0, streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
+  ctx.fill()
+  ctx.globalCompositeOperation = 'destination-in'
+  ctx.drawImage(icons['mdi-play'], 0, 0, streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
 
-  const tomatoCanvas = document.createElement('canvas')
-  const tomatoCtx = tomatoCanvas.getContext('2d', { willReadFrequently: true })
-  const tomatoPadding = 2
-  tomatoCtx.drawImage(tomatoSvg, tomatoPadding, tomatoPadding, streamDeck.ICON_SIZE - tomatoPadding * 2, streamDeck.ICON_SIZE - tomatoPadding * 2)
+  const pauseIconCanvas = new window.OffscreenCanvas(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
+  ctx = pauseIconCanvas.getContext('2d', { willReadFrequently: true })
+  ctx.fillStyle = '#FBBD23'
+  ctx.fillRect(0, 0, streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
+  ctx.fill()
+  ctx.globalCompositeOperation = 'destination-in'
+  ctx.drawImage(icons['mdi-pause'], 0, 0, streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
+
+  const tomatoIconCanvas = new window.OffscreenCanvas(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
+  ctx = tomatoIconCanvas.getContext('2d', { willReadFrequently: true })
+  ctx.fillStyle = '#00000000'
+  ctx.fillRect(0, 0, streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
+  ctx.fill()
+  ctx.drawImage(icons.tomato, 5, 5, streamDeck.ICON_SIZE - 10, streamDeck.ICON_SIZE - 10)
 
   const playIndex = 0
   const pauseIndex = streamDeck.KEY_COLUMNS
   const nextIndex = streamDeck.KEY_ROWS * streamDeck.KEY_COLUMNS - 1
-  for (let i = 0; i < streamDeck.NUM_KEYS; i++) {
-    if (i !== playIndex && i !== pauseIndex && i !== nextIndex) {
-      await streamDeck.fillKeyCanvas(i, tomatoCanvas)
-    }
-  }
   await streamDeck.fillKeyCanvas(playIndex, playCanvas)
+  await streamDeck.fillKeyCanvas(1, playIconCanvas)
+  await streamDeck.fillKeyCanvas(2, pauseIconCanvas)
   await streamDeck.fillKeyCanvas(pauseIndex, pauseCanvas)
+  await streamDeck.fillKeyCanvas(4, tomatoIconCanvas)
   await streamDeck.fillKeyCanvas(nextIndex, nextCanvas)
 
   streamDeck.on('down', async (keyIndex) => {
@@ -114,25 +142,25 @@ const setupStreamDeck = async () => {
 }
 
 document.addEventListener('alpine:init', async () => {
-  const undefinedMedium = new window.FontFace(font, `url(../assets/fonts/${fontPath}.woff2)`)
-  await undefinedMedium.load()
-  document.fonts.add(undefinedMedium)
+  await initialize()
 
-  let streamDeck = window.streamDeck = await setupStreamDeck()
+  const conn = Alpine.store('conn')
+  let streamDeck = await setupStreamDeck(conn)
 
   navigator.hid.addEventListener('connect', async (event) => {
     if (!streamDeck) {
-      streamDeck = window.streamDeck = await setupStreamDeck()
+      streamDeck = await setupStreamDeck(conn)
     }
   })
 
   navigator.hid.addEventListener('disconnect', async (event) => {
     if (streamDeck?.device?.device?.device === event.device) {
       await streamDeck.close()
-      streamDeck = window.streamDeck = null
+      streamDeck = null
     }
   })
 
+  // TODO better hooking to force waiting
   window.addEventListener('beforeunload', async () => {
     if (streamDeck) {
       await streamDeck.resetToLogo()
