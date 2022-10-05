2a4600c29c50840619cfcb6939cf741b38b98f45
David Cooper <david@dtcooper.com>
2022-10-05 16:22:31 -0400

    Invert all keypresses

--- client/src/streamdeck/index.js
+++ client/src/streamdeck/index.js
@@ -28,7 +28,20 @@ const initialize = async () => {
   await Promise.all(iconLoadPromises)
 }
 
-const renderText = (text, iconSize, verticalPadding = 8, horizontalPadding = 7, verticalLinePadding = 3) => {
+const invertCanvas = (canvas) => {
+  const invertedCanvas = new window.OffscreenCanvas(canvas.width, canvas.height)
+  const invertedCtx = invertedCanvas.getContext('2d', { willReadFrequently: true })
+
+  invertedCtx.drawImage(canvas, 0, 0)
+  invertedCtx.fillStyle = 'white'
+  invertedCtx.globalCompositeOperation = 'difference'
+  invertedCtx.globalAlpha = 1
+  invertedCtx.fillRect(0, 0, canvas.width, canvas.height)
+  invertedCtx.fill()
+  return invertedCanvas
+}
+
+const renderText = (text, iconSize, verticalPadding = 8, horizontalPadding = 7, verticalLinePadding = 4) => {
   const canvas = new window.OffscreenCanvas(iconSize, iconSize)
   const ctx = canvas.getContext('2d', { willReadFrequently: true })
 
@@ -67,17 +80,7 @@ const renderText = (text, iconSize, verticalPadding = 8, horizontalPadding = 7, 
     ctx.restore()
   }
 
-  const invertedCanvas = new window.OffscreenCanvas(iconSize, iconSize)
-  const invertedCtx = invertedCanvas.getContext('2d', { willReadFrequently: true })
-
-  invertedCtx.drawImage(canvas, 0, 0)
-  invertedCtx.fillStyle = 'white'
-  invertedCtx.globalCompositeOperation = 'difference'
-  invertedCtx.globalAlpha = 1
-  invertedCtx.fillRect(0, 0, iconSize, iconSize)
-  invertedCtx.fill()
-
-  return { canvas, invertedCanvas }
+  return canvas
 }
 
 const setupStreamDeck = async (conn) => {
@@ -88,9 +91,7 @@ const setupStreamDeck = async (conn) => {
   const streamDeck = await openDevice(streamDecks[0])
   await streamDeck.clearPanel()
   await streamDeck.setBrightness(100)
-  const { canvas: playNextCanvas, invertedCanvas: playNextInvertedCanvas } = renderText('Tomato\nRadio\nAuto-\nmation', streamDeck.ICON_SIZE, 8, 5, 3)
-  const { canvas: pauseCanvas, invertedCanvas: pauseInvertedCanvas } = renderText('PAUSE', streamDeck.ICON_SIZE)
-  const { canvas: playCanvas, invertedCanvas: playInvertedCanvas } = renderText('PLAY', streamDeck.ICON_SIZE)
+
   const playIconCanvas = new window.OffscreenCanvas(streamDeck.ICON_SIZE, streamDeck.ICON_SIZE)
   let ctx = playIconCanvas.getContext('2d', { willReadFrequently: true })
   ctx.fillStyle = '#36D399'
@@ -114,34 +115,30 @@ const setupStreamDeck = async (conn) => {
   ctx.fill()
   ctx.drawImage(icons.tomato, 5, 5, streamDeck.ICON_SIZE - 10, streamDeck.ICON_SIZE - 10)
 
-  const playIndex = 0
-  const pauseIndex = streamDeck.KEY_COLUMNS
-  const nextIndex = streamDeck.KEY_ROWS * streamDeck.KEY_COLUMNS - 1
-  await streamDeck.fillKeyCanvas(playIndex, playNextCanvas)
-  await streamDeck.fillKeyCanvas(1, playIconCanvas)
-  await streamDeck.fillKeyCanvas(2, pauseIconCanvas)
-  await streamDeck.fillKeyCanvas(pauseIndex, pauseCanvas)
-  await streamDeck.fillKeyCanvas(4, tomatoIconCanvas)
-  await streamDeck.fillKeyCanvas(nextIndex, playCanvas)
+  const canvases = [
+    renderText('PLAY\nSTOPSET\nNOW!', streamDeck.ICON_SIZE),
+    playIconCanvas,
+    renderText('PAUSE', streamDeck.ICON_SIZE),
+    pauseIconCanvas,
+    tomatoIconCanvas,
+    renderText('PLAY', streamDeck.ICON_SIZE),
+  ]
+
+  const invertedCanvases = canvases.map((canvas) => invertCanvas(canvas))
+
+  for (let i = 0; i < canvases.length; i++) {
+    await streamDeck.fillKeyCanvas(i, canvases[i])
+  }
 
   streamDeck.on('down', async (keyIndex) => {
-    if (keyIndex === playIndex) {
-      await streamDeck.fillKeyCanvas(keyIndex, playNextInvertedCanvas)
-    } else if (keyIndex === pauseIndex) {
-      await streamDeck.fillKeyCanvas(keyIndex, pauseInvertedCanvas)
-    } else if (keyIndex === nextIndex) {
-      await streamDeck.fillKeyCanvas(keyIndex, playInvertedCanvas)
-    }
+    const row = Math.floor(keyIndex / streamDeck.KEY_COLUMNS) + 1
+    const column = (keyIndex % streamDeck.KEY_COLUMNS) + 1
+    console.log(`Pressed ${row}:${column}`)
+    await streamDeck.fillKeyCanvas(keyIndex, invertedCanvases[keyIndex])
   })
 
   streamDeck.on('up', async (keyIndex) => {
-    if (keyIndex === playIndex) {
-      await streamDeck.fillKeyCanvas(keyIndex, playNextCanvas)
-    } else if (keyIndex === pauseIndex) {
-      await streamDeck.fillKeyCanvas(keyIndex, pauseCanvas)
-    } else if (keyIndex === nextIndex) {
-      await streamDeck.fillKeyCanvas(keyIndex, playCanvas)
-    }
+    await streamDeck.fillKeyCanvas(keyIndex, canvases[keyIndex])
   })
 
   streamDeck.on('error', async (error) => {
