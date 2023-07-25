9e2e94bf92d572b13e25cd85b5d940d2ea592cdc
David Cooper <david@dtcooper.com>
2023-07-25 16:10:07 -0400

    fix bug

--- client/src/stores/connection.js
+++ client/src/stores/connection.js
@@ -80,9 +80,7 @@ export const login = (username, password, host) => {
         auth.update(($auth) => {
           return { ...$auth, connecting: false }
         })
-        if (!sentAuth || !gotAuthResponse) {
-          ws.close()
-        }
+        ws.close()
       }
     }
     ws.onmessage = (e) => {
