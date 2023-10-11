d37295db6d8da8e60d936832a85ff473ee703e44
David Cooper <david.cooper@burningman.org>
2023-10-11 14:55:12 -0400

    Electron now supports wayland out-of-the-box!

--- client/forge.config.js
+++ client/forge.config.js
@@ -1,35 +1,8 @@
-const fs = require("fs")
-const path = require("path")
-const process = require("process")
-
-const afterExtract = []
-const extraLinuxFiles = ["start-tomato.sh"]
-
-if (process.platform === "linux") {
-  // Override executable to start-tomato.sh for AppImage
-  const appBuilder = require("app-builder-lib/out/util/appBuilder")
-  const oldExecuteAppBuilderAsJson = appBuilder.executeAppBuilderAsJson
-  appBuilder.executeAppBuilderAsJson = (args) => {
-    const config = JSON.parse(args[args.length - 1])
-    config.executableName = "start-tomato.sh"
-    args[args.length - 1] = JSON.stringify(config)
-    return oldExecuteAppBuilderAsJson(args)
-  }
-
-  afterExtract.push((buildPath, electronVersion, platform, arch, done) => {
-    for (const file of extraLinuxFiles) {
-      fs.copyFileSync(path.join("scripts/debian", file), path.join(buildPath, file))
-    }
-    done()
-  })
-}
-
 module.exports = {
   packagerConfig: {
     ignore: ["^/src$", "^/scripts$", "^/forge.config.js$", "^/tailwind.config.js$"],
     executableName: "tomato",
-    icon: "assets/icons/tomato",
-    afterExtract
+    icon: "assets/icons/tomato"
   },
   makers: [
     {
@@ -52,7 +25,6 @@ module.exports = {
           icon: "assets/icons/tomato.png",
           desktopTemplate: "./scripts/debian/tomato.desktop",
           section: "sound",
-          bin: "start-tomato.sh",
           scripts: {
             postinst: "./scripts/debian/postinst.sh"
           }
