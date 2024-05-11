be61bb924ac7b2cb2f61195a16cbac6860297ff4
David Cooper <david.cooper@burningman.org>
2024-05-10 22:15:22 -0400

    Tiny config refactor

--- controller/config.py
+++ controller/config.py
@@ -5,14 +5,14 @@ import board
 # Will be true if button is pressed or "debug" is sent via serial/MIDI command
 DEBUG = False
 
-# Enable auto-reload when DEBUG = True
-AUTORELOAD = True
-
 # Pin number for button
 BUTTON_PIN = board.GP15
 
 # Pin number for LED
 LED_PIN = board.GP22
+
+# Enable auto-reload when DEBUG = True
+AUTORELOAD = True
 
 # Brightness settings for LED (these defaults should be okay)
 LED_PWM_MIN_DUTY_CYCLE = 0x1000
