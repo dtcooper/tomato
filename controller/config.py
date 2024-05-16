eaa199541fa98ff6f9977cb0c79a6beb6b11870f
David Cooper <david@dtcooper.com>
2024-05-16 16:59:14 -0400

    Final value for button box

--- controller/config.py
+++ controller/config.py
@@ -6,15 +6,15 @@ import board
 DEBUG = False
 
 # Pin number for button
-BUTTON_PIN = board.GP15
+BUTTON_PIN = board.GP17
 
 # Pin number for LED
-LED_PIN = board.GP22
+LED_PIN = board.GP16
 
 # Enable auto-reload when DEBUG = True
 AUTORELOAD = True
 
 # Brightness settings for LED (these defaults should be okay)
-LED_PWM_MIN_DUTY_CYCLE = 0x1000
+LED_PWM_MIN_DUTY_CYCLE = 0x0000
 LED_PWM_MAX_DUTY_CYCLE = 0xFFFF
-LED_PWM_FREQUENCY = 60
+LED_PWM_FREQUENCY = 150
