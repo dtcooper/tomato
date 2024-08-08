796a7672fce221b16139f83b54cb051833bd0d62
David Cooper <david@dtcooper.com>
2024-08-08 13:44:28 -0400

    Clock UI tweaks

--- client/src/main/Clock.svelte
+++ client/src/main/Clock.svelte
@@ -28,20 +28,30 @@
 
   $: if (isWide) {
     classes =
-      "min-[1500px]:bottom-0 min-[1500px]:right-0 min-[1500px]:rounded-tl-lg min-[1500px]:flex-col min-[1500px]:rounded-bl-none min-[1500px]:pt-3 min-[1500px]:px-3 min-[1500px]:pb-1"
+      "min-[1525px]:bottom-0 min-[1525px]:right-0 min-[1525px]:rounded-tl-lg min-[1525px]:flex-col min-[1525px]:rounded-bl-none min-[1525px]:pt-3 min-[1525px]:px-3 min-[1525px]:pb-1"
   } else {
     classes =
-      "min-[1125px]:bottom-0 min-[1125px]:right-0 min-[1125px]:rounded-tl-lg min-[1125px]:flex-col min-[1125px]:rounded-bl-none min-[1125px]:pt-3 min-[1125px]:px-3 min-[1125px]:pb-1"
+      "min-[1145px]:bottom-0 min-[1145px]:right-0 min-[1145px]:rounded-tl-lg min-[1145px]:flex-col min-[1145px]:rounded-bl-none min-[1145px]:pt-3 min-[1145px]:px-3 min-[1145px]:pb-1"
   }
 </script>
 
 {#if $userConfig.clock}
   <div class="relative h-0">
     <div
-      class="fixed right-0 flex w-max items-end gap-1 rounded-bl-lg bg-base-200 p-2 font-mono leading-none {classes}"
+      class="fixed right-0 flex w-max items-end gap-1 rounded-bl-lg bg-base-200 px-2 py-1 font-mono leading-none {classes}"
     >
-      <div class="hidden {isWide ? 'min-[1500px]:block' : 'min-[1125px]:block'}">{date}</div>
-      <div class="text-lg font-bold {isWide ? 'min-[1500px]:text-2xl' : 'min-[1125px]:text-2xl'}">{time}</div>
+      <div class="hidden {isWide ? 'min-[1525px]:block' : 'min-[1145px]:block'}">{date}</div>
+      <div
+        class="text-lg font-bold {is24Hour
+          ? isWide
+            ? 'min-[1525px]:text-3xl'
+            : 'min-[1145px]:text-3xl'
+          : isWide
+            ? 'min-[1525px]:text-2xl'
+            : 'min-[1145px]:text-2xl'}"
+      >
+        {time}
+      </div>
     </div>
   </div>
 {/if}
