69918968d52eb3701f4da9a8145c5081d59f46d3
David Cooper <david.cooper@burningman.org>
2024-09-20 14:43:15 -0400

    Dockerfile cleanups

--- server/nginx/image/docker-entrypoint.d/25-jinja2-on-templates.sh
+++ server/nginx/image/docker-entrypoint.d/25-jinja2-on-templates.sh
@@ -15,7 +15,6 @@ auto_j2() {
   local template_dir="${NGINX_J2_TEMPLATE_DIR:-/etc/nginx/templates}"
   local suffix="${NGINX_J2_TEMPLATE_SUFFIX:-.j2}"
   local output_dir="${NGINX_J2_OUTPUT_DIR:-/etc/nginx/conf.d}"
-  local env_file="${NGINX_J2_ENV_FILE:-/.env}"
 
   local template relative_path output_path subdir
   [ -d "$template_dir" ] || return 0
