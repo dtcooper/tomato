ec9019657c5c063052df60a2f32ae84c285be0fa
David Cooper <david@dtcooper.com>
2025-09-28 15:55:35 -0400

    Notify constance changes

--- server/tomato/admin/config.py
+++ server/tomato/admin/config.py
@@ -1,13 +1,28 @@
+import logging
+
 from constance.admin import Config, ConstanceAdmin as ConstanceConfigAdmin
 from constance.apps import ConstanceConfig
+from constance.forms import ConstanceForm as OriginalConstanceForm
+
+from ..utils import notify_api
 
 
 Config._meta.verbose_name = Config._meta.verbose_name_plural = "configuration"
 ConstanceConfig.verbose_name = "Settings"
 
 
+logger = logging.getLogger(__name__)
+
+
+class ConstanceForm(OriginalConstanceForm):
+    def save(self):
+        logger.debug("Constance config was saved in admin UI, notifying API")
+        notify_api()
+        super().save()
+
+
 class ConfigAdmin(ConstanceConfigAdmin):
-    # TODO: intercept these for api notification
+    change_list_form = ConstanceForm
 
     def has_view_permission(self, request, obj=None):
         return True
